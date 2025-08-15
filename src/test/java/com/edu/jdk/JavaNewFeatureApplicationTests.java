d9edfeb41942e2cc0cc6dcd846fb4c6810e3f343
ancion <ancion@github.com>
2025-08-15 11:36:18 +0800

    update toolchain

--- src/test/java/com/edu/jdk/JavaNewFeatureApplicationTests.java
+++ src/test/java/com/edu/jdk/JavaNewFeatureApplicationTests.java
@@ -1,13 +1,10 @@
 package com.edu.jdk;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JavaNewFeatureApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
